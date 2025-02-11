c26aed99880242823b8ed106880eb8d9cd63df5f
sumaiya2427 <sumaiya2427@gmail.com>
2025-02-11 12:54:22 +0600

    string and object

---
+++ case.js
@@ -0,0 +1,18 @@
+// const school = "uttara model school";
+// console.log(school.toLocaleUpperCase());
+// uppercase lower case hole,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+// let subject = "chemistry";
+// let book = "CHEMISTY";
+// if (subject.toLocaleLowerCase === book.toLocaleLowerCase) {
+//   console.log("im reading book");
+// } else {
+//   console.log("hudai hudai page ultai");
+// }
+// string e age pore gap hole trim er use,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+let drink = "water";
+let liquid = " water";
+if (drink.trim() === liquid.trim()) {
+  console.log("panir opor nam jibon");
+} else {
+  console.log(" somudre pani ace");
+}
---
+++ slice.js
@@ -0,0 +1,20 @@
+// slice duye vag kora,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+// const address = "andorKilla";
+// const part = address.slice(2, 5);
+// console.log(part);
+
+// split duye vag kora,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+// const sentence = "im a hard worker";
+// console.log(sentence.split(","));
+
+// join use ,,,,,,,,,,,,,,,,,,,,,,
+// const friendtr = ["rahim", "tamim", "fahim", "sarim"];
+// console.log(friendtr.join("$"));
+
+// concat mane aksathe arekta word add kora
+const firstName = "Abid";
+const lastName = "Nabid";
+const fullName = firstName + " " + lastName;
+console.log(fullName);
+// inclide word find
+console.log(fullName.includes("N"));
